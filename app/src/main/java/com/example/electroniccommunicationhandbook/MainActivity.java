2cde7a5d59ad1ee9048dc6750a348b89ec1fed0f
yenlee38 <ngocyen174308@gmail.com>
2021-05-22 20:29:12 +0700

    rebase main

--- app/src/main/java/com/example/electroniccommunicationhandbook/MainActivity.java
+++ app/src/main/java/com/example/electroniccommunicationhandbook/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.electroniccommunicationhandbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.electroniccommunicationhandbook.ui.student.card.CardActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AppCompatButton btn_student_card;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView(){
+        btn_student_card = findViewById(R.id.btn_student_card);
+
+        btn_student_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CardActivity.class);
+                startActivity(intent);
+                setContentView(R.layout.activity_card_student);
+            }
+        });
     }
 }
