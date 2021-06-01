281971ad0d6af84d542c85b8d74f665b2d27941c
NguyenHoang292211 <nguyenhoang13166@gmail.com>
2021-06-01 11:56:53 +0700

    fix conflict

--- app/src/main/java/com/example/electroniccommunicationhandbook/MainActivity_teacher.java
+++ app/src/main/java/com/example/electroniccommunicationhandbook/MainActivity_teacher.java
@@ -25,13 +25,9 @@ public class MainActivity_teacher extends AppCompatActivity {
     AppCompatButton btnClass, btnSchedule, btnLogout;
 
     AppCompatButton btn_createNotification;
-<<<<<<< HEAD
     AppCompatButton btn_offRequest;
     TextView tv_username;
-
-=======
-    AppCompatButton btn_offRequest, btn_viewNotification;
->>>>>>> 7935826...  complete teacher role
+    AppCompatButton  btn_viewNotification;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,11 +38,8 @@ public class MainActivity_teacher extends AppCompatActivity {
         btnLogout = findViewById(R.id.btn_logout_teacher);
         btn_createNotification = findViewById(R.id.btn_notification_new);
         btn_offRequest = findViewById(R.id.btn_send_off_request);
-<<<<<<< HEAD
         tv_username= findViewById(R.id.tv_username_teacher);
-=======
         btn_viewNotification= findViewById(R.id.btn_notification);
->>>>>>> 7935826...  complete teacher role
         initView();
         getInfo();
     }
@@ -78,21 +71,7 @@ public class MainActivity_teacher extends AppCompatActivity {
                 startActivity(intent);
                 finish();
 
-                btn_createNotification.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(getApplicationContext(), TeacherNotificationActivity.class);
-                        startActivity(intent);
-                    }
-                });
 
-                btn_offRequest.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(getApplicationContext(), OffRequestActivity.class);
-                        startActivity(intent);
-                    }
-                });
             }
         });
 
@@ -103,6 +82,23 @@ public class MainActivity_teacher extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btn_createNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), TeacherNotificationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_offRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), OffRequestActivity.class);
+                startActivity(intent);
+            }
+        });
+        
     }
     public void getInfo(){
         UserLocalStore userLocalStore;
